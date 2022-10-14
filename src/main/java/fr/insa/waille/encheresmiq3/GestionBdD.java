f86816b90886917a2cdd66c5eefc9888cf3969ba
grego_9h4zwhb <grego_9h4zwhb@172.17.10.76>
2022-10-14 12:59:25 +0200

    demandeUtilisateur terminée

--- src/main/java/fr/insa/waille/encheresmiq3/GestionBdD.java
+++ src/main/java/fr/insa/waille/encheresmiq3/GestionBdD.java
@@ -258,7 +258,7 @@ public class GestionBdD {
         } 
     }
     
-    public static void creeUtilisateur(Connection con)
+    public static void creeUtilisateur(Connection con,String nom,String prenom,String pass,String email,String code_postal)
             throws SQLException {
         con.setAutoCommit(false);
         try (PreparedStatement pst = con.prepareStatement(
@@ -266,6 +266,23 @@ public class GestionBdD {
                     insert into utilisateur (nom, prenom, pass, email, code_postal)
                     values (?, ?, ?, ?, ?)
                     """)) {
+            pst.setString(1, nom);
+            pst.setString(2, prenom);
+            pst.setString(3, pass);
+            pst.setString(4, email);
+            pst.setString(5,code_postal);
+            pst.executeUpdate();
+            con.commit();
+            con.setAutoCommit(true);
+        } catch (SQLException ex) {
+            con.rollback();
+            throw ex;
+        } finally {
+            con.setAutoCommit(true);
+        }
+    }
+        public static void demandeUtilisateur(Connection con)
+            throws SQLException {
             System.out.println("nom utilisateur :");
             String nom = Lire.S();
             System.out.println("prenom utilisateur:");
@@ -276,11 +293,195 @@ public class GestionBdD {
             String email = Lire.S();
             System.out.println("code_postal utilisateur :");
             String code_postal = Lire.S(); 
+            creeUtilisateur(con,nom,prenom,pass,email,code_postal);
+            
+    }
+    
+    public static void afficheEncheres(Connection con)
+                throws SQLException{
+        con.setAutoCommit(false);
+        try(Statement st = con.createStatement()){
+            ResultSet resultats = st.executeQuery(
+                    """
+                    ---ordre SQL pour récupérer la liste des encheres:
+                    select * from encheres
+                    """
+            );
+            System.out.println("Liste des encheres :");
+            while(resultats.next()){
+                int id = resultats.getInt("id");
+                String quand = resultats.getString("quand");
+                int montant = resultats.getInt("montant");
+                int de = resultats.getInt("de");
+                int sur = resultats.getInt("sur");              
+                System.out.println(id+" : "+quand+" "+montant+"€ "+de+" "+sur);
+            }
+        }
+        catch (SQLException ex) {
+            // quelque chose s'est mal passé
+            // j'annule la transaction
+            con.rollback();
+            // puis je renvoie l'exeption pour qu'elle puisse éventuellement
+            // être gérée (message à l'utilisateur...)
+            throw ex;
+        } finally {
+            // je reviens à la gestion par défaut : une transaction pour
+            // chaque ordre SQL
+            con.setAutoCommit(true);
+        }
+    }
+    
+    public static void creeEnchere(Connection con)
+                throws SQLException {
+        con.setAutoCommit(false);
+        try (PreparedStatement pst = con.prepareStatement(
+        """
+                    insert into enchere(quand, montant, de, sur)
+                    values (?, ?, ?, ?, ?)
+                    """)) {
+            System.out.println("quand :");
+            String quand = Lire.S();
+            System.out.println("montant:");
+            int montant = Lire.i();
+            System.out.println("de :");
+            int de = Lire.i();
+            System.out.println("sur :");
+            int sur = Lire.i();
+            pst.setString(1, quand);
+            pst.setInt(2, montant);
+            pst.setInt(3, de);
+            pst.setInt(4, sur);
+            pst.executeUpdate();
+            con.commit();
+            con.setAutoCommit(true);
+        } catch (SQLException ex) {
+            con.rollback();
+            throw ex;
+        } finally {
+            con.setAutoCommit(true);
+     }
+    }
+    
+    public static void afficheCategorie(Connection con)
+            throws SQLException{
+        con.setAutoCommit(false);
+        try(Statement st = con.createStatement()){
+            ResultSet resultats = st.executeQuery(
+                    """
+                    ---ordre SQL pour récupérer la liste des categories :
+                    select * from categorie
+                    """
+            );
+            System.out.println("Liste des categories :");
+            while(resultats.next()){
+                int id = resultats.getInt("id");
+                String nom = resultats.getString("nom");
+                System.out.println(id+" : "+nom);
+            }
+        }
+        catch (SQLException ex) {
+            // quelque chose s'est mal passé
+            // j'annule la transaction
+            con.rollback();
+            // puis je renvoie l'exeption pour qu'elle puisse éventuellement
+            // être gérée (message à l'utilisateur...)
+            throw ex;
+        } finally {
+            // je reviens à la gestion par défaut : une transaction pour
+            // chaque ordre SQL
+            con.setAutoCommit(true);
+        } 
+    }
+    
+    public static void creeCategorie(Connection con)
+            throws SQLException {
+        con.setAutoCommit(false);
+        try (PreparedStatement pst = con.prepareStatement(
+        """
+                    insert into categorie (nom)
+                    values (?)
+                    """)) {
+            System.out.println("nom categorie :");
+            String nom = Lire.S();
             pst.setString(1, nom);
-            pst.setString(2, prenom);
-            pst.setString(3, pass);
-            pst.setString(4, email);
-            pst.setString(5,code_postal);
+            pst.executeUpdate();
+            con.commit();
+            con.setAutoCommit(true);
+        } catch (SQLException ex) {
+            con.rollback();
+            throw ex;
+        } finally {
+            con.setAutoCommit(true);
+        }
+    }
+    
+    public static void afficheObjets(Connection con)
+            throws SQLException{
+        con.setAutoCommit(false);
+        try(Statement st = con.createStatement()){
+            ResultSet resultats = st.executeQuery(
+                    """
+                    ---ordre SQL pour récupérer la liste des objets:
+                    select * from objet
+                    """
+            );
+            System.out.println("Liste des objets :");
+            while(resultats.next()){
+                int id = resultats.getInt("id");
+                String titre = resultats.getString("titre");
+                String description = resultats.getString("description");
+                String debut = resultats.getString("debut");
+                String fin = resultats.getString("fin");              
+                String prix_base = resultats.getString("prix_base");
+                int categorie = resultats.getInt("categorie");
+                int propose_par = resultats.getInt("propose_par");
+                System.out.println(id+" : "+titre+" "+description+" "+debut+" "+fin+" "+prix_base+" "+categorie+" "+propose_par);
+            }
+        }
+        catch (SQLException ex) {
+            // quelque chose s'est mal passé
+            // j'annule la transaction
+            con.rollback();
+            // puis je renvoie l'exeption pour qu'elle puisse éventuellement
+            // être gérée (message à l'utilisateur...)
+            throw ex;
+        } finally {
+            // je reviens à la gestion par défaut : une transaction pour
+            // chaque ordre SQL
+            con.setAutoCommit(true);
+        }
+    }
+
+    public static void creeObjet(Connection con)
+            throws SQLException {
+        con.setAutoCommit(false);
+        try (PreparedStatement pst = con.prepareStatement(
+        """
+                    insert into objet (titre, description, debut, fin, prix_base, categorie, propose_par)
+                    values (?, ?, ?, ?, ?, ?, ?)
+                    """)) {
+            System.out.println("titre objet:");
+            String titre = Lire.S();
+            System.out.println("description objet:");
+            String description = Lire.S();
+            System.out.println("debut vente:");
+            String debut = Lire.S();
+            System.out.println("fin vente:");
+            String fin = Lire.S();
+            System.out.println("prix de base objet :");
+            int prix_base = Lire.i();
+            System.out.println("categorie objet :");
+            int categorie = Lire.i();
+            System.out.println("objet proposé par :");
+            int propose_par = Lire.i();
+            
+            pst.setString(1, titre);
+            pst.setString(2, description);
+            pst.setString(3, debut);
+            pst.setString(4, fin);
+            pst.setInt(5,prix_base);
+            pst.setInt(6,categorie);
+            pst.setInt(7,propose_par);
             pst.executeUpdate();
             con.commit();
             con.setAutoCommit(true);
@@ -301,6 +502,12 @@ public class GestionBdD {
             System.out.println("2 - Suppression du schéma");
             System.out.println("3 - Affichage liste utilisateurs");
             System.out.println("4 - Ajouter un utilisateur");
+            System.out.println("5 - Affichage liste categories");
+            System.out.println("6 - Ajouter une categorie");
+            System.out.println("7 - Affichage liste encheres");
+            System.out.println("8 - Ajouter une enchere");
+            System.out.println("9 - Affichage liste objets");
+            System.out.println("10 - Ajouter un objet");
             System.out.println("99 - Quitter");
             int reponse=-1; //reponse entrée par l'utilisateur
             while(reponse<0){
@@ -321,8 +528,32 @@ public class GestionBdD {
                         System.out.println("utilisateurs récupérés OK");
                         break;
                     case 4 :
-                        creeUtilisateur(con);
+                        demandeUtilisateur(con);
                         System.out.println("utilisateur créé OK");
+                        break;
+                    case 5 :
+                        afficheCategorie(con);
+                        System.out.println("categories récupérées OK");
+                        break;
+                    case 6 :
+                        creeCategorie(con);
+                        System.out.println(" categories créées OK");
+                        break;
+                    case 7 :
+                        afficheEncheres(con);
+                        System.out.println("encheres récupérées OK");
+                        break;
+                    case 8 :
+                        creeEnchere(con);
+                        System.out.println(" encheres créées OK");
+                        break;  
+                    case 9 :
+                        afficheObjets(con);
+                        System.out.println("objets récupérés OK");
+                        break;
+                    case 10 :
+                        creeObjet(con);
+                        System.out.println(" objet créés OK");
                         break;  
                     case 99 :
                         stop = true;
@@ -357,6 +588,6 @@ public class GestionBdD {
         } catch (SQLException ex) {
             throw new Error(ex);
         }
-    }
+}
     
 }
