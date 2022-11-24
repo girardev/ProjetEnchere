3c7110488050ab2501895b90379adeea1a55662f
BlaskGDT <56971719+BlaskGDT@users.noreply.github.com>
2022-11-24 15:57:17 +0100

    all

--- src/main/java/fr/insa/waille/encheresmiq3/GUIFX/GridPaneAuthentification.java
+++ src/main/java/fr/insa/waille/encheresmiq3/GUIFX/GridPaneAuthentification.java
@@ -3,76 +3,166 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package fr.insa.waille.encheresmiq3.GUIFX;
-
-import java.awt.event.ActionEvent;
-import javafx.event.EventHandler;
+import static fr.insa.waille.encheresmiq3.bdd.GestionBdD.creeUtilisateur;
+import static fr.insa.waille.encheresmiq3.bdd.GestionBdD.defautConnect;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 
 /**
  *
  * @author arvar
- */
+ */    
 public class GridPaneAuthentification extends GridPane {
-    public GridPaneAuthentification(){
+
+    public GridPaneAuthentification(Stage stage){
         //PARTIE INSCRIPTION
         //création des labels et champs de saisie
         Label titre = new Label("Inscription");
+        titre.setStyle("-fx-max-width: 50");
+        titre.setStyle("-fx-font-weight: bold");
         Label labelNom = new Label("nom");
-        TextField nom = new TextField();
+        TextField Fnom = new TextField();
         Label labelPrenom = new Label("prenom");
-        TextField prenom = new TextField();
+        TextField Fprenom = new TextField();
         Label labelPass = new Label("password");
-        TextField pass = new TextField();
+        PasswordField Fpass = new PasswordField();
         Label labelEmail = new Label("email");
-        TextField email = new TextField();
+        TextField Femail = new TextField();
         Label labelCodePostal = new Label("code postal");
-        TextField CodePostal = new TextField();
+        TextField Fcodepostal = new TextField();
         Button B_Inscription = new Button("Inscription");
         Label panneau = new Label();
         
-        B_Inscription.setOnAction((t) ->{
-            System.out.println("coucou");
-        });
-                
-                
-                
         
         //ajout des composants au GridPane (colonne,ligne)
         this.add(titre,0,0);
         this.add(labelNom, 0, 1);
-        this.add(nom, 1, 1);
+        this.add(Fnom, 1, 1);
         this.add(labelPrenom, 0, 2);
-        this.add(prenom, 1, 2);
-        this.add(labelPass, 0, 3);
-        this.add(pass, 1, 3);
-        this.add(labelEmail, 0, 4);
-        this.add(email, 1, 4);
+        this.add(Fprenom, 1, 2);
+        this.add(labelEmail, 0, 3);
+        this.add(Femail, 1, 3);
+        this.add(labelPass, 0, 4);
+        this.add(Fpass, 1, 4);
         this.add(labelCodePostal, 0, 5);
-        this.add(CodePostal, 1, 5);
-        this.add(B_Inscription, 1, 6);
+        this.add(Fcodepostal, 1, 5);
+        this.add(B_Inscription, 1, 6);  
         this.add(panneau,1,7);
-       
-
+        
+        //action de l'appuie sur le bouton inscription
+        B_Inscription.setOnAction((t) ->{
+            String nom = Fnom.getText();
+            String prenom = Fprenom.getText();
+            String pass = Fpass.getText();
+            String email = Femail.getText();
+            String codepostal = Fcodepostal.getText();
+            
+            //initialisation de Connection con
+            Connection con = null;
+            try {
+                con = defautConnect();
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(GridPaneAuthentification.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(GridPaneAuthentification.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            //utilisation de la fonction creeUtilisateur
+            try {
+                creeUtilisateur(con,nom,prenom,pass,email,codepostal);
+            } catch (SQLException ex) {
+                Logger.getLogger(GridPaneAuthentification.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            Fnom.setText("");
+            Fprenom.setText("");
+            Fpass.setText("");
+            Femail.setText("");
+            Fcodepostal.setText("");
+            
+            panneau.setText("Inscription effectuée");
+        });
+        
+        
         
         
         
         //PARTIE CONNEXION
         //création des labels et champs de saisie
         Label titre2 = new Label("Connexion");
+        titre2.setStyle("-fx-max-width: 50");
+        titre2.setStyle("-fx-font-weight: bold");
         Label labelPass2 = new Label("password");
-        TextField pass2 = new TextField();
+        PasswordField Fpass2 = new PasswordField();
         Label labelEmail2 = new Label("email");
-        TextField email2 = new TextField();
+        TextField Femail2 = new TextField();
+        Button B_Connexion = new Button("Connexion");
+        Label panneau2 = new Label();
+        
         //ajout des composants au GridPane (colonne,ligne)
         this.add(titre2,2,0);
-        this.add(labelPass2, 2, 1);
-        this.add(pass2, 3, 1);
-        this.add(labelEmail2, 2, 2);
-        this.add(email2, 3, 2);
+        this.add(labelEmail2, 2, 1);
+        this.add(Femail2, 3, 1);
+        this.add(labelPass2, 2, 2);
+        this.add(Fpass2, 3, 2);
+        this.add(B_Connexion, 2, 6);
+        this.add(panneau2,2,7);
+        
+        
+        //action de l'appuie sur le bouton connexion
+        B_Connexion.setOnAction((t) ->{
+            String pass2 = Fpass2.getText();
+            String email2 = Femail2.getText();
+            
+            //initialisation de Connection con
+            Connection con = null;
+            try {
+                con = defautConnect();
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(GridPaneAuthentification.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(GridPaneAuthentification.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            //utilisation recherche du mot de passe d'un email
+            try(Statement st1 = con.createStatement()){
+                String query1 = "select pass from utilisateur where email like '"+email2+"' ";
+                ResultSet resultat = st1.executeQuery(query1);
+                String motdepasse = null;
+                while(resultat.next()){
+                motdepasse = resultat.getString("pass");
+                }
+                
+                if(motdepasse.equals(pass2)){          
+                    Femail2.setText("");
+                    Fpass2.setText("");
+                    panneau2.setText("Connexion possible");
+                    
+                    Scene sc2 = new Scene(new Accueil());
+                    //Scene sc = new Scene(new TestF));
+                    stage.setScene(sc2);
+                }
+                else{
+                    panneau2.setText("Connexion impossible");
+                }
+            
+            } catch (SQLException ex) {
+                Logger.getLogger(GridPaneAuthentification.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
 
     }
 }
+
